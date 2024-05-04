11810fdaf1d0062c9aa0b92da32fa4aac23167de
Ben <vantonderbenna@gmail.com>
2024-05-04 12:56:17 +0200

    implemented sections to scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import IntroPage from "./pages/IntroPage";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const scrollToTop = () => {
+      window.scrollTo(0, 0);
+    };
+
+    scrollToTop();
+  }, [pathname]);
+
+  return null;
+};
+
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/IntroPage" element={<IntroPage />} />
