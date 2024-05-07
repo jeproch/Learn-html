b3b038922d10217430f60a77be3a1dcdb35480cd
Ben <vantonderbenna@gmail.com>
2024-05-07 19:08:49 +0200

    contentCommit

---
+++ src/elementspage components/ElementsBody.tsx
@@ -0,0 +1,23 @@
+const ElementsBody = () => {
+  return (
+    <div className=" mx-2">
+      <h2 className="text-2xl font-semibold text-primary mb-4">
+        Understanding the Body Element
+      </h2>
+      <p className="text-lg text-gray-700 leading-relaxed mb-4">
+        In web development, the <code className="text-muted">{"<body>"}</code>{" "}
+        element serves as the foundational container for all content displayed
+        on a webpage. It encapsulates everything visible to the user, including
+        text, images, and interactive elements.
+      </p>
+      <p className="text-lg text-gray-700 leading-relaxed">
+        Simply put, the <code className="text-muted">{"<body>"}</code> tag
+        houses the entire visual and interactive content of a website, providing
+        the canvas upon which the web page is constructed and displayed to
+        visitors.
+      </p>
+    </div>
+  );
+};
+
+export default ElementsBody;
--- src/pages/ElementsPage.tsx
+++ src/pages/ElementsPage.tsx
@@ -1,11 +1,13 @@
 import IntroTopBar from "../intropage components/IntroTopBar";
 import ElementsHeading from "../elementspage components/ElementsHeading";
+import ElementsBody from "../elementspage components/ElementsBody";
 
 const ElementsPage = () => {
   return (
     <>
       <IntroTopBar></IntroTopBar>
       <ElementsHeading></ElementsHeading>
+      <ElementsBody></ElementsBody>
     </>
   );
 };
